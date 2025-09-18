9b18e15bd30911718bf67b83e2aacf3cd7acc561
Leo <lauravalescalemos02@gmail.com>
2025-09-18 08:41:35 -0300

    Task.java

--- src/com/exemplo/todo/Task.java
+++ src/com/exemplo/todo/Task.java
@@ -1,1 +1,29 @@
+package com.exemplo.todo;
+
+public class Task {
+    private String title;
+    private boolean completed;
+
+    public Task(String title) {
+        this.title = title;
+        this.completed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void markCompleted() {
+        this.completed = true;
+    }
+
+    @Override
+    public String toString() {
+        return (completed ? "[X] " : "[ ] ") + title;
+    }
+}
 
