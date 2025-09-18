ad3041444af5ec313ec10764e52bc7f95a5d8c6b
Leo <lauravalescalemos02@gmail.com>
2025-09-18 08:41:52 -0300

    TaskManager.java

--- src/com/exemplo/todo/TaskManager.java
+++ src/com/exemplo/todo/TaskManager.java
@@ -1,1 +1,39 @@
+package com.exemplo.todo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManager {
+    private List<Task> tasks;
+
+    public TaskManager() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(String title) {
+        tasks.add(new Task(title));
+    }
+
+    public void markTaskCompleted(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.get(index).markCompleted();
+        }
+    }
+
+    public void removeTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.remove(index);
+        }
+    }
+
+    public void listTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("Nenhuma tarefa registrada.");
+        } else {
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println(i + 1 + ". " + tasks.get(i));
+            }
+        }
+    }
+}
 
